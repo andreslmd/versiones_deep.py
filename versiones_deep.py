45859651fc098d1295b19dfb9f3f47e2206c516b
Andy L <94586171+andreslmd@users.noreply.github.com>
2022-05-18 23:11:16 -0300

    Add files via upload

---
+++ versiones_deep.py
@@ -0,0 +1,9 @@
+#tensorflow
+import tensorflow
+print('tensorflow: %s' %tensorflow.__version__)
+#keras
+import keras
+print('keras: %s' %keras.__version__)
+#tfkeras
+import tensorflow,keras
+print('tensorflow.keras: %s' %tensorflow.keras.__version__)
