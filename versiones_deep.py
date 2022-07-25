394baa1572464a1ecca39fb008ee8f97d7f6d9bd
Andy L <94586171+andreslmd@users.noreply.github.com>
2022-07-25 00:23:02 -0300

    Update versiones_deep.py

--- versiones_deep.py
+++ versiones_deep.py
@@ -1,9 +1,6 @@
 #tensorflow
 import tensorflow
 print('tensorflow: %s' %tensorflow.__version__)
-#keras
-import keras
-print('keras: %s' %keras.__version__)
 #tfkeras
-import tensorflow,keras
+import tensorflow.keras
 print('tensorflow.keras: %s' %tensorflow.keras.__version__)
